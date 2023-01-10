b27c27e10e2315336eaf3b7bfbff5e5d40a67655
poojaaccount <pcchavan750@gmail.com>
2023-01-10 12:25:37 -0300

    crud_demo10 backup

---
+++ src/main/java/com/crud/CrudDemo10Application.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemo10Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/crud/entities/Students.java
@@ -0,0 +1,5 @@
+package com.crud.entities;
+
+public class Students {
+
+}
